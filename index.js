89e94cf4942b21c1b8d45d5c182e007bb698f3e5
Bobby Hinton <bob.g.hint@gmail.com>
2022-04-15 21:38:11 -0500

    commit tree 02099c502daad4fd1eeb1f968352629183d28c05

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name) {
+    name = cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    name = cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    name = cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    name = cats.shift(); 
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
